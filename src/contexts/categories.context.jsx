c18d7be0869039429bcd191f2542ee39b6106f52
davidolaoluwa240 <davidolaoluwa240@gmail.com>
2023-05-18 02:00:17 +0200

    Integrated graphql and apollo client

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,24 +1,42 @@
-import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { createContext, useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
+  loading: false,
 });
 
+const COLLECTIONS = gql`
+  query GetCollections {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+    if (!data) return;
+    const { collections } = data;
+    const collectionsMap = collections.reduce((acc, collection) => {
+      const { title, items } = collection;
+      acc[title.toUpperCase()] = items;
+      return acc;
+    }, {});
+    setCategoriesMap(collectionsMap);
+  }, [data]);
 
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
-import { CartProvider } from './contexts/cart.context';
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import { CategoriesProvider } from "./contexts/categories.context";
+import { CartProvider } from "./contexts/cart.context";
 
-import './index.scss';
+import "./index.scss";
 
-const rootElement = document.getElementById('root');
+const client = new ApolloClient({
+  uri: "https://crwn-clothing.com/",
+  cache: new InMemoryCache(),
+});
 
-render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
+      <ApolloProvider client={client}>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </ApolloProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
